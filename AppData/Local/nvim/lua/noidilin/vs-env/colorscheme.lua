cf18369fc79ab5fab7af36088e4e3688331b89fa
noidilin <linganinja.0120@gmail.com>
2024-10-14 02:14:34 +0800

    chore: remove outdate theme files

--- AppData/Local/nvim/lua/noidilin/vs-env/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "rebelot/kanagawa.nvim",
-  priority = 1000,                         -- Make sure to load this before all the other start plugins.
-  config = function()
-    vim.cmd.colorscheme("kanagawa-dragon") -- set color scheme
-  end,
-}
