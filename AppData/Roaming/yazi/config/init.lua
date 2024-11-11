5274da5a4c09620aef5e42467ad118615e5ed282
noidilin <linganinja.0120@gmail.com>
2024-11-12 02:29:20 +0800

    feat(yazi): add plugins
    - smart paste
    - symlink in status
    - user name and host name in header
    - display size and modified time

---
+++ AppData/Roaming/yazi/config/init.lua
@@ -0,0 +1,39 @@
+-- https://yazi-rs.github.io/docs/tips/#symlink-in-status
+-- show symlink in status bar
+function Status:name()
+	local h = self._tab.current.hovered
+	if not h then
+		return ui.Line({})
+	end
+
+	local linked = ""
+	if h.link_to ~= nil then
+		linked = " -> " .. tostring(h.link_to)
+	end
+	return ui.Line(" " .. h.name .. linked)
+end
+
+-- https://yazi-rs.github.io/docs/tips/#username-hostname-in-header
+-- show username and hostname in header
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ui.Line({})
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
+
+-- linemode: size_and_mtime
+-- ~/.config/yazi/init.lua
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.modified or 0)
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = os.date("%b %d  %Y", time)
+	end
+
+	local size = self._file:size()
+	return ui.Line(string.format("%s %s", size and ya.readable_size(size) or "-", time))
+end
