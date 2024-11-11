66b4088766d2b50334999a6a7b888bd58b9b90e4
noidilin <linganinja.0120@gmail.com>
2024-11-12 02:21:04 +0800

    fix: update directory path to match current user folder name

--- AppData/Local/nvim/lua/plugins/lush.lua
+++ AppData/Local/nvim/lua/plugins/lush.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"rktjmp/lush.nvim",
 		{
-			dir = "C:\\Users\\Lin\\AppData\\Local\\nvim\\lua\\plugins\\color-fatigue",
+			dir = "C:\\Users\\noidilin\\AppData\\Local\\nvim\\lua\\plugins\\color-fatigue",
 			lazy = true,
 		},
 	},
