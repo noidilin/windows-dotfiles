e7a58bd62bb39a439876ba2abb43393dbeb56498
noidilin <linganinja.0120@gmail.com>
2025-02-17 20:12:01 +0800

    chore(yazi): remove smart-paste plugin

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -1,26 +1,22 @@
--- https://yazi-rs.github.io/docs/tips/#symlink-in-status
+-- https://yazi-rs.github.io/docs/tips#symlink-in-status
 -- show symlink in status bar
-function Status:name()
-	local h = self._tab.current.hovered
-	if not h then
-		return ui.Line({})
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
 	end
+end, 3300, Status.LEFT)
 
-	local linked = ""
-	if h.link_to ~= nil then
-		linked = " -> " .. tostring(h.link_to)
-	end
-	return ui.Line(" " .. h.name .. linked)
-end
-
--- https://yazi-rs.github.io/docs/tips/#username-hostname-in-header
+-- https://yazi-rs.github.io/docs/tips#username-hostname-in-header
 -- show username and hostname in header
--- Header:children_add(function()
--- 	if ya.target_family() ~= "unix" then
--- 		return ui.Line({})
--- 	end
--- 	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
--- end, 500, Header.LEFT)
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ""
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
 
 -- linemode: size_and_mtime
 -- ~/.config/yazi/init.lua
