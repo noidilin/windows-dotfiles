15fb5d51ccc05c2406fbf3673edb1d5b084e57db
noidilin <linganinja.0120@gmail.com>
2025-03-07 22:00:14 +0800

    fix(bat): fix bat scoop bucket overrides env variable

--- dot_install/symlinks.ps1
+++ dot_install/symlinks.ps1
@@ -7,6 +7,8 @@ $symlinks = @{
     "$env:APPDATA\Rime" = "$HOME\.config\rime"
     "$env:APPDATA\vivid" = "$HOME\.config\vivid"
     "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" = "$HOME\.config\winterm\settings.json"
+    # make sure my config is used even $env:BAT_CONFIG_DIR is changed by bat scoop bucket
+    "$HOME\scoop\persist\bat" = "$HOME\.config\bat"
     # for vscode-neovim, since it currently (2024-11-27) won't launch neovim from XDG_CONFIG_HOME
     "$env:LOCALAPPDATA\nvim"                                                                      = "$HOME\.config\nvim"
     # "$HOME\AppData\Local\fastfetch"                                                                 = ".\fastfetch"
